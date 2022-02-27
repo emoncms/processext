ef0700d971f01d7bf16e2105dd252e778ec25a2b
TrystanLea <trystan.lea@gmail.com>
2022-02-27 20:27:23 +0000

    Delayed kWh to power input processor by @rmn30 https://github.com/emoncms/emoncms/pull/1686

--- processext_processlist.php
+++ processext_processlist.php
@@ -25,25 +25,37 @@ class processext_ProcessList
     public function process_list() {
         
         $list = array(
-           array(
-              "name"=>_("pow10 input"),
-              "short"=>"s inp",
-              "argtype"=>ProcessArg::INPUTID,
-              "function"=>"pow10_input",
-              "datafields"=>0,
-              "unit"=>"",
-              "group"=>_("Input"),
-              "description"=>_("<p>Scales the current value by ten to the power of the other input.</p>")
-           ),
-           array(
-              "name"=>_("value and scale"),
-              "short"=>"v+s",
-              "argtype"=>ProcessArg::NONE,
-              "function"=>"value_and_scale",
-              "datafields"=>0,
-              "unit"=>"",
-              "group"=>_("Calibration"),
-              "description"=>_("<p>Unpack combined value and scale factor, applying the scale factor.</p>")
+            array(
+                "name"=>_("pow10 input"),
+                "short"=>"s inp",
+                "argtype"=>ProcessArg::INPUTID,
+                "function"=>"pow10_input",
+                "datafields"=>0,
+                "unit"=>"",
+                "group"=>_("Input"),
+                "description"=>_("<p>Scales the current value by ten to the power of the other input.</p>")
+            ),
+            array(
+                "name"=>_("value and scale"),
+                "short"=>"v+s",
+                "argtype"=>ProcessArg::NONE,
+                "function"=>"value_and_scale",
+                "datafields"=>0,
+                "unit"=>"",
+                "group"=>_("Calibration"),
+                "description"=>_("<p>Unpack combined value and scale factor, applying the scale factor.</p>")
+            ),
+            array(
+                "name"=>_("kWh to Power (smoothed)"),
+                "short"=>"kwhpwr_smooth",
+                "argtype"=>ProcessArg::FEEDID,
+                "function"=>"kwh_to_power_smoothed",
+                "datafields"=>1,
+                "unit"=>"W",
+                "group"=>_("Power & Energy"),
+                "engines"=>array(Engine::PHPFINA,Engine::PHPTIMESERIES),
+                "requireredis"=>true,
+                "description"=>_("<p>Convert accumulating kWh to instantaneous power, averaging over the last 10 input values. This is useful when the kWh measurement is derived from pulse counting because a short (e.g. 5s) sampling interval leads to quantisation which causes the power level to appear to oscillate between coarse-grained levels (720W for 1 Wh pulses counted for 5s). This filter delays power output for 10 samples in order to give more fine grained output. Quantisation becomes less apparent but changes of power level are delayed slightly and spread over the expanded window.</p>")
            )
         );
         return $list;
@@ -76,5 +88,40 @@ class processext_ProcessList
     {
         $input_value = $this->input->get_last_value($id);
         return $value * pow(10, $input_value);
+    }
+
+    //---------------------------------------------------------------------------------------
+    // Delayed / smoothed kWh to power input processor by Robert Norton
+    // See: https://github.com/emoncms/emoncms/pull/1686
+    //---------------------------------------------------------------------------------------
+    public function kwh_to_power_smoothed($feedid,$time,$value)
+    {
+        global $redis;
+        if (!$redis) return $value; // return if redis is not available
+
+        $power = 0;
+        $queue_len = 10;
+        // fetch the oldest samples in the queue
+        $last_time = $redis->lIndex("process:kwhtopower_smoothed:$feedid:times",-1);
+        $last_value = $redis->lIndex("process:kwhtopower_smoothed:$feedid:values",-1);
+        // Push the new sample and trim to $queue_len entries (actually queue_len+1 but shrug)
+        $redis->lPush("process:kwhtopower_smoothed:$feedid:times", $time);
+        $redis->lTrim("process:kwhtopower_smoothed:$feedid:times", 0, $queue_len);
+        $redis->lPush("process:kwhtopower_smoothed:$feedid:values", $value);
+        $redis->lTrim("process:kwhtopower_smoothed:$feedid:values", 0, $queue_len);
+        if (($last_time !== false) && ($last_value !== false)) { // will fail on first call
+            // compute the power between now and queue_len samples ago
+            $kwhinc = $value - $last_value;
+            $joules = $kwhinc * 3600000.0;
+            $timeelapsed = ($time - $last_time);
+            if ($timeelapsed>0) {     //This only avoids a crash, it's not ideal to return "power = 0" to the next process.
+                $power = $joules / $timeelapsed;
+                $this->feed->insert_data($feedid, $time, $time, $power);
+            } else {
+                $this->log->error("Time elapsed was not greater than zero: $timeelapsed.");
+            }
+        }
+
+        return $power;
     }
 }
